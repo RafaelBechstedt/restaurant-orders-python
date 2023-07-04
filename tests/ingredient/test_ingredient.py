f09a35df75ae292c5a069204921c73a954f6614c
Rafael <bechstedt@hotmail.com>
2023-07-04 20:17:19 -0300

    Req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,25 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from models.ingredient import Ingredient, Restriction
 
 
-# Req 1
 def test_ingredient():
-    pass
+    # Teste inicialização ingredient
+    ingredient = Ingredient("queijo mussarela")
+    assert ingredient.name == "queijo mussarela"
+    assert ingredient.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED}
+
+    # Teste __repr__
+    assert repr(ingredient) == "Ingredient('queijo mussarela')"
+
+    # Teste __eq__
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("queijo mussarela")
+    ingredient3 = Ingredient("farinha")
+    assert ingredient1 == ingredient2
+    assert ingredient1 != ingredient3
+    assert ingredient1 == ingredient1
+
+    # Teste __hash__
+    assert hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) != hash(ingredient3)
